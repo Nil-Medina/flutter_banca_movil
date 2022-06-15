2bc1c666f93a76359872bc3bd9b80a1623726e14
KEVIN-JOSUE <71499421@certus.edu.pe>
2022-06-14 23:36:38 -0500

    user 2

--- lib/screen/perfil.dart
+++ lib/screen/perfil.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 //import 'package:flutter_banca_movil/componentes/BarNavigationHome.dart';
 
 class PagePerfil extends StatelessWidget {
@@ -10,41 +12,127 @@ class PagePerfil extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Container(
-          alignment: const Alignment(-0.3, 0),
-          child: const Text(
-            "PERFIL",
-            style: TextStyle(color: Colors.white),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Center(child: Text("PERFIL ")),
+            backgroundColor: Colors.black,
           ),
-        ),
-        backgroundColor: Colors.black,
-      ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        //shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        children: <Widget>[
-          Container(
-            height: 620,
-            width: 300,
-            decoration: const BoxDecoration(color: Colors.grey),
-            child: WillPopScope(
-              onWillPop: () {
-                _backpage(context);
-                return Future.value(false);
-              },
-              child: const Center(
-                child: Text(
-                  "Proximamente",
-                  style: TextStyle(fontSize: 45),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 220,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/lugar.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 80,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/perfilpreview.png"),
+                          fit: BoxFit.cover,
+                        ),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 4,
+                        ),
+                      ),
+                      margin: EdgeInsets.only(bottom: 10),
+                    ),
+                    Text(
+                      "Jean Pierre Vasquez",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                  ],
                 ),
               ),
-            ),
-          )
-        ],
-      ),
-    );
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                color: Color.fromARGB(255, 56, 55, 55),
+                padding: EdgeInsets.symmetric(vertical: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      //mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("NOTIFICACIONES",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              //Alignment.topLeft,
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                color: Color.fromARGB(255, 56, 55, 55),
+                padding: EdgeInsets.symmetric(vertical: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      //mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Registro de transacciones",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              //Alignment.topLeft,
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                color: Color.fromARGB(255, 56, 55, 55),
+                padding: EdgeInsets.symmetric(vertical: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      //mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Registro de Retiro",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              //Alignment.topLeft,
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
