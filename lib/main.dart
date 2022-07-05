a17bff53732c2221add98b8e9a15d9b84041f016
KEVIN-JOSUE <71499421@certus.edu.pe>
2022-07-05 11:28:09 -0500

    notication

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 //import 'package:http/http.dart' as http;
+import 'package:flutter_application_banca/service/push_notification.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_banca/view/inicio.dart';
 import 'package:flutter_application_banca/view/login.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 //import 'package:shared_preferences/shared_preferences.dart';
 //import 'package:flutter_application_banca/screen/inicio.dart';
 
 //import 'componentes/BarNavigationHome.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await PushNotificationService.initializeApp();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
---
+++ lib/service/push_notification.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class PushNotificationService {
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+  static String? token;
+
+  static Future initializeApp() async {
+    //await Firebase.initializeApp();
+
+    token = await FirebaseMessaging.instance.getToken();
+    print('token: $token');
+  }
+}
